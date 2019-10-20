245e91b4d9b037a4c0e3312197333979c931f6f6
Anonymous666666 <56450117+Anonymous666666@users.noreply.github.com>
2019-10-20 14:01:29 -0700

    Update Main.java

--- src/Homework5/Main.java
+++ src/Homework5/Main.java
@@ -5,10 +5,10 @@ public class Main {
         String[] habits = {"run", "eat", "sleep"};
         String[][] schedule = {{"Sunday", "Code"}, {"Monday", "Code"}};
 
-        Human mother = new Human("Katy", "william", 1968, 100, schedule);
-        Human father = new Human("Edward", "Johnson", 1956, 100, schedule);
+        Human mother = new Human("Katy", "william", 1973, 100, schedule);
+        Human father = new Human("Edward", "Johnson", 1955, 100, schedule);
         Pet pet = new Pet("dog", "Ildirim", 3, 100, habits);
-        Human child = new Human("Steven", "Mattson", 1993, 100, schedule);
+        Human child = new Human("Steven", "Mattson", 1992, 100, schedule);
 
         Family family = new Family(mother, father, 1);
         family.addChild(child);
